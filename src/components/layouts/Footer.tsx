d6bf74ae94a814bde209e41529048344808058e1
jcapshawaz <jcapshawaz@gmail.com>
2024-07-17 00:52:41 +0000

    Updated npm install

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='container text-center mt-3'>
       <section className='border-top d-inline-block p-2'>
-        rDuckDev © {new Date().getFullYear()} | All rights reserved
+        Recon Radar by Dealer Acumen © {new Date().getFullYear()} | All rights reserved
       </section>
     </footer>
   );
